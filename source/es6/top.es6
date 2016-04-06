ad01eb551cdbd77346df39018f226648a2a25448
ハトネコエ <hato.nekoe@gmail.com>
2016-04-06 12:03:33 -1000

    jQuery でなくて Zepto にしてみた

--- source/es6/top.es6
+++ source/es6/top.es6
@@ -2,3 +2,23 @@ window.addEventListener("load", function(){
 	"use strict";
 
 }, false);
+
+// jQuery の基本
+$(function(){
+	$(".output").css("color", "blue");
+});
+
+// イベント処理
+/*
+function doSomething(){
+	console.log("Hello");
+}
+*/
+
+$(function(){
+	$(".test-button").click(function(){
+		$(".output").css("color", "red");
+		$(this).css("color", "red");
+		console.log("Hello");
+	});
+});
--- source/top.js
+++ source/top.js
@@ -1,3 +1,23 @@
 "use strict";
 
 window.addEventListener("load", function () {}, false);
+
+// jQuery の基本
+$(function () {
+	$(".output").css("color", "blue");
+});
+
+// イベント処理
+/*
+function doSomething(){
+	console.log("Hello");
+}
+*/
+
+$(function () {
+	$(".test-button").click(function () {
+		$(".output").css("color", "red");
+		$(this).css("color", "red");
+		console.log("Hello");
+	});
+});
