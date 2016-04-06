2ced6ad1b396040baff49577fa04c4ffdf10a78d
ハトネコエ <hato.nekoe@gmail.com>
2016-04-06 13:10:06 -1000

    トランプクラスの仕組みを作成

--- source/es6/top.es6
+++ source/es6/top.es6
@@ -1,18 +1,65 @@
-window.addEventListener("load", function(){
-	"use strict";
-
-}, false);
-
-$(function(){
+$(function()
+{
 	// jqBasicTest();
 	// jqHandlerTest();
 	algo1();
 });
 
-function algo1(){
+// １：順次処理
+function algo1()
+{
 	var x = 10;
 	var y = x + 5;
 	$(".output").text("y = " + y);
+
+	var cards = initCards();
+	console.log( cards );
+	$(".output").text( cards[5].displayCard() );
+}
+
+// Joker を除く 52 枚のカードを生成
+function initCards()
+{
+	var cards = [];
+	for(var suitId = 0; suitId < 4; ++suitId)
+	{
+		var suit;
+		switch (suitId) {
+			case 0:
+				suit = "♠";
+				break;
+			case 1:
+				suit = "♣";
+				break;
+			case 2:
+				suit = "♥";
+				break;
+			case 3:
+				suit = "♦";
+				break;
+		}
+		for(var i = 1; i <= 13; ++i)
+		{
+			cards.push(new Card(suit, i));
+		}
+	}
+	return cards;
+}
+
+// トランプクラス
+class Card
+{
+	constructor(_suit, _number)
+	{
+		this.suit   = _suit;
+		this.number = _number;
+	}
+
+	// カードの文字と番号を返す
+	displayCard()
+	{
+		return ("[" + this.suit + "" + this.number + "]");
+	}
 }
 
 // jQuery の基本
--- source/top.js
+++ source/top.js
@@ -1,6 +1,8 @@
 "use strict";
 
-window.addEventListener("load", function () {}, false);
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 $(function () {
 	// jqBasicTest();
@@ -8,13 +10,69 @@ $(function () {
 	algo1();
 });
 
+// １：順次処理
 function algo1() {
 	var x = 10;
 	var y = x + 5;
 	$(".output").text("y = " + y);
+
+	var cards = initCards();
+	console.log(cards);
+	$(".output").text(cards[5].displayCard());
 }
 
+// Joker を除く 52 枚のカードを生成
+function initCards() {
+	var cards = [];
+	for (var suitId = 0; suitId < 4; ++suitId) {
+		var suit;
+		switch (suitId) {
+			case 0:
+				suit = "♠";
+				break;
+			case 1:
+				suit = "♣";
+				break;
+			case 2:
+				suit = "♥";
+				break;
+			case 3:
+				suit = "♦";
+				break;
+		}
+		for (var i = 1; i <= 13; ++i) {
+			cards.push(new Card(suit, i));
+		}
+	}
+	return cards;
+}
+
+// トランプクラス
+
+var Card = function () {
+	function Card(_suit, _number) {
+		_classCallCheck(this, Card);
+
+		this.suit = _suit;
+		this.number = _number;
+	}
+
+	// カードの文字と番号を返す
+
+
+	_createClass(Card, [{
+		key: "displayCard",
+		value: function displayCard() {
+			return "[" + this.suit + "" + this.number + "]";
+		}
+	}]);
+
+	return Card;
+}();
+
 // jQuery の基本
+
+
 function jqBasicTest() {
 	$(".output").text("<b>output, output</b>");
 	$(".output").after("<b>outpit, outpit</b>");
